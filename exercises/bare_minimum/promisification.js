d8f5bc7fa4654c161b927032035279a28776c424
Lee Hoyong <igotoweb@gmail.com>
2019-03-06 18:33:32 +0900

    fix a response error

--- exercises/bare_minimum/promisification.js
+++ exercises/bare_minimum/promisification.js
@@ -19,7 +19,7 @@ var getGitHubProfile = function(user, callback) {
   request.get(options, function(err, res, body) {
     if (err) {
       callback(err, null);
-    } else if (body.message) {
+    } else if (body && body.message) {
       callback(new Error('Failed to get GitHub profile: ' + body.message), null);
     } else {
       callback(null, body);
