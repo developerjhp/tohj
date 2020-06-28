5c3953bc793a0075b34cfb9f755b5120f7b35c3f
Lee Hoyong <igotoweb@gmail.com>
2020-06-28 19:23:03 +0900

    update submit.js

--- .travis/submit.js
+++ .travis/submit.js
@@ -6,7 +6,7 @@ if (TRAVIS_PULL_REQUEST_SLUG === '\n') {
   throw new Error('github username is missing');
 }
 
-exec('jest ./__test__/index.test.js --json', (err, json, stderr) => {
+exec('jest part-2/__test__/**.test.js --json', (err, json, stderr) => {
   const result = JSON.parse(json);
   const username = TRAVIS_PULL_REQUEST_SLUG.split('/')[0];
 
