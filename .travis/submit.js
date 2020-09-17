6ec15703d5738f17c07f6fb9e92ea1068076a33f
Lee Hoyong <igotoweb@gmail.com>
2020-09-17 16:24:34 +0900

    updated submit code
    # Conflicts:
    #	.travis/submit.js

--- .travis/submit.js
+++ .travis/submit.js
@@ -1,22 +1,36 @@
-const { exec } = require('child_process');
-const https = require('https');
-const { URCLASS_URL, ASSESSMENT_ID, TRAVIS_PULL_REQUEST_SLUG } = process.env;
+const { exec } = require("child_process");
+const https = require("https");
+const {
+  URCLASS_URL,
+  URCLASS_KEY,
+  ASSESSMENT_ID,
+  TRAVIS_PULL_REQUEST_SLUG
+} = process.env;
 
-if (TRAVIS_PULL_REQUEST_SLUG === '\n') {
-  throw new Error('github username is missing');
+if (URCLASS_KEY === "\n") {
+  throw new Error("urclass key is missing");
 }
 
-exec('jest part-2/__test__/**.test.js --json', (err, json, stderr) => {
+if (TRAVIS_PULL_REQUEST_SLUG === "\n") {
+  throw new Error("github username is missing");
+}
+
+
+if (TRAVIS_PULL_REQUEST_SLUG === "\n") {
+  throw new Error("github username is missing");
+}
+
+exec("jest part-2/__test__/**.test.js --json", (err, json, stderr) => {
   const result = JSON.parse(json);
-  const username = TRAVIS_PULL_REQUEST_SLUG.split('/')[0];
+  const username = TRAVIS_PULL_REQUEST_SLUG.split("/")[0];
 
   const options = {
     hostname: URCLASS_URL,
     path: `/production/submit/sprint`,
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-    },
+      "Content-Type": "application/json"
+    }
   };
 
   console.log(JSON.stringify(options));
@@ -25,32 +39,32 @@ exec('jest part-2/__test__/**.test.js --json', (err, json, stderr) => {
   const body = {
     assessment_id: ASSESSMENT_ID,
     githubUsername: username,
-    result: result,
+    result: result
   };
 
   makeRequest(options, body);
 });
 
 function makeRequest(options, body) {
-  const req = https.request(options, (res) => {
+  const req = https.request(options, res => {
     let data;
-    res.on('data', (chunk) => {
+    res.on("data", chunk => {
       data += chunk;
     });
-    res.on('end', () => {
-      console.log('data from urclass is ', data);
+    res.on("end", () => {
+      console.log("data from urclass is ", data);
       if (res.statusCode >= 400) {
         if (res.statusCode === 400) {
-          throw new Error('invalid github username.');
+          throw new Error("invalid github username.");
         }
-        throw new Error('There is an error on response from urclass.');
+        throw new Error("There is an error on response from urclass.");
       }
     });
   });
 
-  req.on('error', (e) => {
+  req.on("error", e => {
     console.log(e);
-    throw new Error('data did not send to urclass');
+    throw new Error("data did not send to urclass");
   });
 
   req.write(JSON.stringify(body));
