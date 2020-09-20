945c1630b5e2601ab6635c22430a9742690061d3
mcwithimp <mcwithimp@gmail.com>
2020-09-21 01:07:07 +0900

    enhanced testcases

--- part-2/__test__/03_basicChaining.test.js
+++ part-2/__test__/03_basicChaining.test.js
@@ -1,7 +1,25 @@
 const fs = require('fs');
-const { readAllUsersChaining } = require("../03_basicChaining");
+const { readAllUsersChaining } = require('../03_basicChaining');
+const sinon = require('sinon');
+sinon.spy(Promise, 'all');
+sinon.spy(fs, 'readFile');
+sinon.spy(fs, 'readFileSync');
+beforeEach(function () {
+  Promise.all.resetHistory();
+  fs.readFile.resetHistory();
+  fs.readFileSync.resetHistory();
+});
 
-describe("Basic chaining Test", () => {
+// delete commnet
+const MULTI_LINES_COMMENT = /\/\*[\s\S]*?\*\/(\r?\n|\r)/;
+const ONE_LINE_COMMENT = /\/\/.*(\r?\n|\r)/;
+const COMMENT = new RegExp(
+  `${MULTI_LINES_COMMENT.source}|${ONE_LINE_COMMENT.source}`,
+  'g'
+);
+const funcBody = readAllUsersChaining.toString().replace(COMMENT, '');
+
+describe('Basic chaining Test', () => {
   describe('readAllUsersChaining', () => {
     test('체이닝의 결과가 Promise 형태로 리턴되어야 합니다', () => {
       const result = readAllUsersChaining();
@@ -9,44 +27,50 @@ describe("Basic chaining Test", () => {
     });
 
     test('user1.json의 내용과 user2.json 내용을 합쳐 객체로 리턴되어야 합니다', (done) => {
-      readAllUsersChaining().then(json => {
+      readAllUsersChaining().then((json) => {
         const userArray = [
           {
-            "name": "김코딩",
-            "age": 26,
-            "sex": "Male",
-            "company": {
-              "name": "코드스테이츠"
-            }
+            name: '김코딩',
+            age: 26,
+            sex: 'Male',
+            company: {
+              name: '코드스테이츠',
+            },
           },
           {
-            "name": "박해커",
-            "age": 40,
-            "sex": "Female",
-            "company": {
-              "name": "Anomymous"
-            }
-          }
-        ]
+            name: '박해커',
+            age: 40,
+            sex: 'Female',
+            company: {
+              name: 'Anomymous',
+            },
+          },
+        ];
         expect(json).toEqual(userArray);
-        done()
-      })
+        done();
+      });
     });
 
-    test('fs module을 직접 사용하는 것이 아닙니다', () => {
-      const contents = fs.readFileSync('./part-2/03_basicChaining.js').toString();
-      expect(contents).not.toMatch('require\("fs"\)', 'g');
-      expect(contents).not.toMatch("require\('fs'\)", 'g');
+    test('fs module을 직접 사용하지 말고, getDataFromFilePromise을 두 번 사용해야 합니다', () => {
+      let matched = funcBody.match(/getDataFromFilePromise\(.+\)/g) || [];
+      expect(matched.length).toBe(2);
+
+      matched =
+        funcBody.match(
+          /fs\.readFile(Sync)?|fs\[(\'|\")readFile(Sync)?(\'|\")\]/g
+        ) || [];
+      expect(matched.length).toBe(0);
     });
 
-    test('getDataFromFilePromise를 활용하세요. 총 두 번 사용해야 합니다', () => {
-      expect(readAllUsersChaining.toString().match(/getDataFromFilePromise/g)).toHaveLength(2);
+    it('Promise.all 또는 async/await을 사용하지 않고 풀어보세요', (done) => {
+      readAllUsersChaining()
+        .then((json) => {
+          expect(Promise.all.called).toBe(false);
+          const matched = funcBody.match(/await/g) || [];
+          expect(matched.length).toBe(0);
+          done();
+        })
+        .catch(done);
     });
-
-    test('Promise.all 또는 async/await 을 사용하지 않고 풀어보세요', () => {
-      expect(readAllUsersChaining.toString()).not.toMatch(/Promise\.all/g);
-      expect(readAllUsersChaining.toString()).not.toMatch(/await/g);
-    })
-  })
-
+  });
 });
